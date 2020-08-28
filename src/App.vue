b11fdee914077da876614d19c468cbab5278b7eb
OLiang24 <68525467+OLiang24@users.noreply.github.com>
2020-08-28 22:22:20 +0700

    jub

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+<template>
+  <div id="app" class="playGameScreen">
+
+      <div class="row a">
+
+        <div class="col-md-5 ">
+          <br>
+          <h1>{{bird_name}}  HP ; {{bird_hp}}</h1>
+          <div class="heroTile tile">
+            <img :src="b_img" alt class="img-fluid image" :width="bird_hp + 'px'" />
+          </div>
+        </div>
+        <div class="col-md-2 ">
+          <h1><img src="./assets/img/vs.png" alt="" width="100%"></h1>
+        </div>
+        <div class="col-md-5">
+        <br>
+          <h1>{{pig_name}}  HP ; {{pig_hp}}</h1>
+          <div class="enemyTile tile">
+            <img :src="p_img" alt class="img-fluid image" :width="pig_hp + 'px'" />
+          </div>
+        </div>
+      </div>
+
+      <game
+        @B_name="b_name"
+        @B_hp="b_hp"
+        @B_image="b_image"
+        @P_name="p_name"
+        @P_hp="p_hp"
+        @P_image="p_image"
+        @B_atk="b_atk"
+        @P_atk="p_atk"
+        @B_SATK="b_satk"
+        @P_SATK="p_satk"
+      ></game>
+
+  </div>
+</template>
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import game from "./components/game";
+export default {
+  name: "App",
+  components: {
+    game,
+  },
+  data: function () {
+    return {
+      bird_name: "",
+      pig_name: "",
+      bird_hp: 0,
+      pig_hp: 0,
+      attack: 0,
+      b_img: "",
+      p_img: "",
+    };
+  },
+  methods: {
+    //hero
+    b_name(value) {
+      this.bird_name = value;
+      console.log("emit", this.bird_name);
+    },
+    b_hp(value) {
+      this.bird_hp = value;
+      console.log("emit", this.bird_hp);
+    },
+    b_image(value) {
+      this.b_img = value;
+      console.log("emit", this.b_img);
+    },
+
+    // Mon
+    p_name(value) {
+      this.pig_name = value;
+    },
+    p_hp(value) {
+      this.pig_hp = value;
+      
+    },
+    p_image(value) {
+      this.p_img = value;
+      
+
+    },
+    //Attack hero
+    b_atk(value) {
+      this.attack = value;
+      this.pig_hp -= this.attack;
+      console.log("emit", this.pig_hp);
+    },
+    b_satk(value) {
+      this.attack = value;
+      this.pig_hp -= this.attack;
+    },
+    //Attack mon
+    p_atk(value) {
+      this.attack = value;
+      this.bird_hp -= this.attack;
+    },
+
+    p_satk(value) {
+      this.attack = value;
+      this.bird_hp -= this.attack;
+    },
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.playGameScreen {
+  background-image: url("assets/img/bg.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+.a {
+  color: rgb(255, 64, 143);
+  font-family: 'Lobster', cursive;
+  margin: auto;
+  width: 90%;
+  height: 650px;
+}
+.tiles {
+  position: absolute;
+  bottom: 5%;
+  width: 100%;
+}
+.tile {
+  display: inline-block;
+  margin: 5px;
+  bottom: 0;
+}
+.heroTile {
+  float: center;
+}
+.enemyTile {
+  float: bottom;
+}
+</style>
---
+++ src/components/game.vue
@@ -0,0 +1,109 @@
+<template>
+<div>
+    <button v-on:click="Start()" class="button-a btn btn-outline-info " id="start">Start</button>
+    <button v-on:click="Attack()" class="button-a btn btn-outline-success" id="atk">Attack</button> 
+    <button v-on:click="Special()" class="button-a btn btn-outline-danger" id="atk">Special Attack</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      //     aplayer: { name: " player", hp: 1000, image: "" },
+      //     amonster: { name: "monster", hp: 1000, image: "" },
+      player: [
+        { name: "Red", hp: 200, image: require("../assets/img/red.png") },
+        { name: "Chuck ", hp: 225, image: require("../assets/img/chuck.png") },
+        { name: "Bomb", hp: 250, image: require("../assets/img/bomb.png") },
+        { name: "Matilda", hp: 275, image: require("../assets/img/matilda.png") },
+        { name: "Terence", hp: 300, image: require("../assets/img/terence.png") },
+      ],
+
+      monster: [
+        { name: "pigs", hp: 200, image: require("../assets/img/pig.png") },
+        { name: "fermale pigs", hp: 225, image: require("../assets/img/female.png") },
+        { name: "armor pigs", hp: 250, image: require("../assets/img/armor.png") },
+        { name: "mushed pigs", hp: 275, image: require("../assets/img/mushed.png") },
+        { name: "king pigs", hp: 300, image: require("../assets/img/king.png") },
+      ],
+    };
+  },
+  methods: {
+    randomm: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+
+    Start: function () {
+      //1.random player 1-3
+      //2.random monster 1-3
+      this.randomB = this.randomm(1, 5) - 1;
+      this.randomP = this.randomm(1, 5) - 1;
+
+      //สุ่มค่า player 1-5 --->ชื่อ ค่า hp รูป
+      this.name_player = this.player[this.randomB].name;
+      this.$emit("B_name", this.name_player);
+      console.log("emit", this.name_player);
+
+      this.hp_player = this.player[this.randomB].hp;
+      this.$emit("B_hp", this.hp_player);
+      console.log("hp", this.hp_player);
+
+      this.image_player = this.player[this.randomB].image;
+      this.$emit("B_image", this.image_player);
+      console.log("im", this.image_player);
+
+      //สุ่มค่า Monster 1-5 --->ชื่อ ค่า hp รูป
+      this.name_monster = this.monster[this.randomP].name;
+      console.log("emit", this.name_monster);
+      this.$emit("P_name", this.name_monster);
+
+      this.hp_monster = this.monster[this.randomP].hp;
+      this.$emit("P_hp", this.hp_monster);
+
+      this.image_monster = this.monster[this.randomP].image;
+      this.$emit("P_image", this.image_monster);
+    },
+
+    Attack: function () {
+      //1.player --> monster
+      this.atk = this.randomm(5, 15);
+      this.$emit("B_atk", this.atk);
+      console.log("Attack :", this.atk);
+
+      this.atk = this.randomm(5, 15);
+      this.$emit("P_atk", this.atk);
+      console.log("Atk back", this.atk);
+
+      //3.player.HP <=0 ===>!
+      //4.monster.HP <=0 ===>!
+      //5.END GAME
+    },
+    Special: function () {
+      this.s_atk = this.randomm(20, 50);
+      this.$emit("B_SATK", this.s_atk);
+      console.log("Special Attack :", this.s_atk);
+
+      //2.player <-- monster
+      this.s_atk = this.randomm(20, 50);
+      this.$emit("P_SATK", this.s_atk);
+      console.log("SPATK back :", this.s_atk);
+    },
+  },
+};
+</script>
+<style >
+.f {
+  font-size: 50px;
+}
+
+.button-a {
+  font-size: 20px;
+  background-color: #f5dbab;
+  border: rgb(238, 25, 149);
+  padding: 10px 10px 10px 10px;
+  text-align: center;
+  display: inline-block;
+  margin: 20px 10px;
+}
+</style>
